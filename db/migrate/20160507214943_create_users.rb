b027f42eb8732966e021fc29810715668b85cf41
unknown <roberto_ironmaiden@live.com.mx>
2016-05-08 07:17:43 -0500

    correccion de conflictos en la migracion

--- db/migrate/20160507214943_create_users.rb
+++ db/migrate/20160507214943_create_users.rb
@@ -1,21 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-<<<<<<< HEAD
       t.string :firstname
       t.string :lastname
       t.string :gender
       t.string :email
       t.string :password_digest
       t.integer :age
-=======
-    	t.string :firstname
-    	t.string :lastname
-    	t.string :gender
-    	t.string :email
-    	t.string :password_digest
-    	t.integer :age
->>>>>>> static_pages
 
       t.timestamps null: false
     end
