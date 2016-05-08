1440bbfde1a64df1815272c88b361fd40380a56b
unknown <roberto_ironmaiden@live.com.mx>
2016-05-07 21:40:54 -0500

    Roberto is gay but made the helpers & treatment controller

--- app/controllers/treatments_controller.rb
+++ app/controllers/treatments_controller.rb
@@ -1,28 +1,19 @@
 class TreatmentsController < ApplicationController
-  before_action :set_treatment, only: [:show, :edit, :update, :destroy]
 
-  # GET /treatments
-  # GET /treatments.json
   def index
-    @treatments = Treatment.all
+    @treatments = current_user.treatments
   end
 
-  # GET /treatments/1
-  # GET /treatments/1.json
   def show
   end
 
-  # GET /treatments/new
   def new
     @treatment = Treatment.new
   end
 
-  # GET /treatments/1/edit
   def edit
   end
 
-  # POST /treatments
-  # POST /treatments.json
   def create
     @treatment = Treatment.new(treatment_params)
 
@@ -32,10 +23,7 @@ class TreatmentsController < ApplicationController
       render :new
     end
   end
-end
 
-  # PATCH/PUT /treatments/1
-  # PATCH/PUT /treatments/1.json
   def update
     if @treatment.update(treatment_params)
       redirect_to @treatment
@@ -44,21 +32,15 @@ end
     end
   end
 
-  # DELETE /treatments/1
-  # DELETE /treatments/1.json
   def destroy
-    respond_to do |format|
-      redirect_to treatments_url
-    end
+    @treatment.find(params[:id]).destroy
+    redirect_to root_path
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_treatment
-      @treatment = Treatment.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def treatment_params
-      params.require(:treatment).permit(:weight, :dose, :medicament)
-    end
+  def treatment_params
+    params.require(:treatment).permit(:weight, :dose, :medicament)
+  end
+
+end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,14 +5,6 @@ module UsersHelper
 		session[:user_id] = user.id
 	end
 
-	# Remembers a user in a persistent session.
-	def remember(user)
-		user.remember
-		cookies.permanent.signed[:user_id] = user.id
-		cookies.permanent[:remember_token] = user.remember_token
-	end
-
-
 	# Returns the current logged-in user (if any).
 	def current_user
 		if (user_id = session[:user_id])
@@ -35,28 +27,10 @@ module UsersHelper
 		!current_user.nil?
 	end
 
-	# Forgets a persistent session.
-	def forget(user)
-		user.forget
-		cookies.delete(:user_id)
-		cookies.delete(:remember_token)
-	end
-
 	def log_out
 		forget(current_user)
 		session.delete(:user_id)
 		@current_user = nil
-	end
-
-	# Redirects to stored location (or to the default).
-	def redirect_back_or(default)
-		redirect_to(session[:forwarding_url] || default)
-		session.delete(:forwarding_url)
-	end
-
-	# Stores the URL trying to be accessed.
-	def store_location
-		session[:forwarding_url] = request.url if request.get?
 	end
 
 end
