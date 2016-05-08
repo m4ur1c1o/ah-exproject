478a74bd6bd3d861c25a2bea72aebe0a917c9d19
Diana E. Ortiz <dian05ab@hotmail.com>
2016-05-07 21:45:44 -0500

    finished_user_crud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
 
 	def create
-				
+		@user = User.new(user_params)
+		if @user.save
+			flash[:info] = 'Usuario creado exitosamente'
+			redirect_to current_user
+		else
+			render 'new'
+		end			
 	end
 
 	def new
@@ -17,12 +23,24 @@ class UsersController < ApplicationController
 	end
 
 	def update
-		
+		@user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+			flash[:success] = 'Perfil actualizados exitosamente'
+			redirect_to current_user
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
 		@user = User.find(params[:id])
 		@user.destroy
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:firstname, :lastname, :age, :gender, :email, :password)
 	end
 
 end
